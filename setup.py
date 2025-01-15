eb7d3e4a25ee2dab158629bab0a9cb79c376094f
Lars Stietz <lars.stietz@tuhh.de>
2025-01-15 17:06:06 +0100

    first test at using pycall to interface

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testmodule',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here, e.g.
+        'numpy',
+        'dedalus',
+    ],
+    author='Lars Stietz',
+    author_email='lars.stietz@tuhh.de',
+    description='A project for solving Bayesian inverse problems',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://collaborating.tuhh.de/l_stz/bayesian-inverse-problems',
+    python_requires='>=3.10'
+)
