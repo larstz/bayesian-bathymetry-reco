d5631cc814fa441862f60208db1e51dd6cb6f515
Lars Stietz <lars.stietz@tuhh.de>
2025-01-24 16:19:23 +0100

    constraint on prior to fit simulation needs, multivariate priot
    Former-commit-id: 45679a7de1d4ef5d199f8b0e2c57921dbab52c8b

--- bathymetry_reco/mcmc_bathymetry_reco.jl
+++ bathymetry_reco/mcmc_bathymetry_reco.jl
@@ -16,30 +16,45 @@ struct observation_data
     h::Array{Float64}
 end
 
+struct simulation_setup
+    xbounds::Tuple{Float64, Float64}
+    timestep::Float64
+    nx::Int
+    tend::Float64
+    g::Float64
+    kappa::Float64
+    dealias::Float64
+end
+
 # Define the Turing model
-@model function shallow_water_model(observation::observation_data)
+@model function shallow_water_model(observation::observation_data, sim_params::simulation_setup;
+                                    sigma::Matrix{Float64}=Matrix(I, 64, 64))
     # Define the prior for the bathymetry peak
     println("Running the model")
     #peak ~ Uniform(0, 10)
     #s ~ Uniform(0.001, 2)
-    # Create SWE solver
-    b ~ MvNormal(zeros(64), 1)
-    xbounds = (0., 10.)
-    nx = 64
-    tend = 10
-    timestep = 1e-3
-    g = 9.81
-    kappa = 0.2
-    dealias = 3/2
 
+    #b ~ MvNormal(zeros(64), 0.1*sigma)
+    b ~ Product(fill(Uniform(0,0.2), sim_params.nx))
+    display(plot!(b, label=""))
+    if maximum(b) > 0.28
+        Turing.@addlogprob! -Inf
+        return nothing
+    end
     # Create SWE solver and calculate the solution
-    solver = swe.SWESolver(xbounds, timestep, nx, tend, g, kappa, dealias);
+    solver = swe.SWESolver(sim_params.xbounds, sim_params.timestep,
+                            sim_params.nx, sim_params.tend, sim_params.g,
+                            sim_params.kappa, sim_params.dealias);
     # Use the solver to simulate the shallow water equation with the sampled peak
     #simulation_h, _, t = solver.solve((peak, s));
     simulation_h, _, t = solver.solve(b);
     x = solver.domain.x
     sim_interp = LinearInterpolation((t,x), simulation_h)
-
+    if maximum(t)<10
+        println("Not simulated until end")
+        display(heatmap(simulation_h, aspect_ratio=1))
+        readline()
+    end
     # interpolate values of the simulation to match the observation
     sim_observations = sim_interp.(observation.t, observation.x')
 
@@ -63,26 +78,63 @@ function load_observation_data(file_path::String)
         sensor_pos = [2., 4., 6., 8.]
         t_measured = collect(0:0.1:10)
         observation_h = obs_interpolated.(t_measured, sensor_pos')
-        noise_dist = Normal(0, 0.1)
-        noise = rand(noise_dist, size(observation_h))
-        observation_h += noise
+        #noise_dist = Normal(0, 0.1)
+        #noise = rand(noise_dist, size(observation_h))
+        #observation_h += noise
 
         return observation_data(collect(t_measured), sensor_pos, observation_h)
     end
 end
 
+function create_laplace_matrix_1d(N)
+    # N ist die Anzahl der Gitterpunkte
+    L = zeros(Float64, N, N)  # Erstellen einer N×N Matrix
+
+    # Füllen der Matrix mit den Werten für den Laplace-Operator
+    for i in 1:N
+        if i > 1
+            L[i, i - 1] = 1  # Nachbar links
+        end
+        L[i, i] = -2  # Zentraler Punkt
+        if i < N
+            L[i, i + 1] = 1  # Nachbar rechts
+        end
+    end
+    return L
+end
+
+save = true
+
 file_path = "./data/toy_measurement/simulation_data.h5"
 observation = load_observation_data(file_path)
 
+# Define the simulation parameters
+xbounds = (0., 10.)
+nx =  64
+tend = 10
+timestep = 1e-3
+g = 9.81
+kappa = 0.2
+dealias = 3/2
+sim_params = simulation_setup(xbounds, timestep, nx, tend, g, kappa, dealias)
+
+# Create the covariance matrix
+L = create_laplace_matrix_1d(nx)
+sigma = 1/nx^2 .*inv(L)inv(L)'
+
 # Instantiate the model
-model = shallow_water_model(observation)
+model = shallow_water_model(observation, sim_params; sigma=sigma)
 
 # Sample from the posterior
-chain = sample(model, MH(), 50, burnin=10)
+n_samples = 50
+burnin = 1
+chain = sample(model, MH(), n_samples, burnin=burnin)
 # Because the solver is written in python we need a gradient free sampler like MH
 # Print the results
 
-serialize("./data/results/chain_soph.jls", chain)
-plot(chain)
-savefig("./plots/mcmc_bathymetry_reco_chain_soph.pdf")
-println(chain)
+if save
+    serialize("./data/results/chain_soph_uniform.jls", chain)
+    plot(chain)
+    savefig("./plots/mcmc_bathymetry_reco_chain_soph_uniform.pdf")
+    println(chain)
+end
--- pycall_test.jl
+++ pycall_test.jl
@@ -3,6 +3,7 @@ Pkg.activate(".")
 
 using Turing
 using PyCall
+using Plots
 
 PyCall.python
 # Import the SWESolver class from the utils module
@@ -30,19 +31,70 @@ file_path = "./data/toy_measurement/simulation_data.h5"
 observation = load_observation_data(file_path)
 
 xbounds = (0., 10.)
-nx = 130 # 64
+nx =  64
 tend = 10
-timestep = 5e-5 # 1e-3
+timestep = 1e-3
 g = 9.81
 kappa = 0.2
 dealias = 3/2
 bathy_peak = (5,1) #rand(MvNormal(zeros(130), 1),1)
-b = observation.b
-println(size(b))
+#b = observation.b[1:2:end-2]
+b = zeros(64)
+b[1] = rand(Normal(0,0.01),1)[1]
+for i in 2:64
+    b[i] = rand(Normal(b[i-1],0.01),1)[1]
+end
+function create_exponential_decay_matrix(n, decay_factor)
+    A = zeros(n, n)
+    for i in 1:n
+        for j in 1:n
+            A[i, j] = exp(-1/decay_factor * abs(i - j))
+        end
+    end
+    return A
+end
+
+# Create laplacian operator matrix with dirichlet boundary conditions
+function create_laplace_matrix_1d(N)
+    # N ist die Anzahl der Gitterpunkte
+    L = zeros(Float64, N, N)  # Erstellen einer N×N Matrix
+
+    # Füllen der Matrix mit den Werten für den Laplace-Operator
+    for i in 1:N
+        if i > 1
+            L[i, i - 1] = 1  # Nachbar links
+        end
+        L[i, i] = -2  # Zentraler Punkt
+        if i < N
+            L[i, i + 1] = 1  # Nachbar rechts
+        end
+    end
+    return 1/N.*L
+end
+
+
+
+decay_factor = 100  # Decay factor for off-diagonals
+A = create_exponential_decay_matrix(nx, decay_factor)
+b = rand(MvNormal(zeros(64), A), 1)
+
+# smoothed prior
+L = create_laplace_matrix_1d(nx)
+gamma = 0.1
+w = rand(MvNormal(zeros(nx), 1), 1)
+b = L\(gamma.*w)
+b = 0.2.* b/maximum(b)
+plot(b)
+savefig("b_rand_smoothed.png")
 # Create SWE solver and calculate the solution
 solver = swe.SWESolver(xbounds, timestep, nx, tend, g, kappa, dealias)
+solver.set_initial_conditions()
+c = 1.2e-3 + minimum(solver.initial_conditions.H)
+println(minimum(solver.initial_conditions.H))
+b=ones(64)*c
 
+solver.set_initial_conditions()
+println(minimum(solver.initial_conditions.H-b))
 # Use the solver to simulate the shallow water equation with the sampled peak
-@time simulation = solver.solve(b)
-
-println(size(simulation[1]))
+@time simulation_h, _, t = solver.solve(b)
+println(t[end])
