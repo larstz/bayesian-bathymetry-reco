60794b64fe655efbead109562d1923d23baa62eb
Lars Stietz <lars.stietz@tuhh.de>
2025-04-09 16:35:52 +0200

    update python toy script to run again
    Former-commit-id: ec69470d20573a7538c19675afa425eb6b834fce

--- toy_measurement.py
+++ toy_measurement.py
@@ -3,7 +3,9 @@ import tomllib
 
 import h5py
 import numpy as np
-
+import sys
+import time
+sys.path.append("./BathymetryReco/src")
 from swe_wrapper import SWESolver, rampFunc, gaussian_bathymetry
 
 
@@ -31,11 +33,13 @@ def main():
     dealias = sim_params["dealias"]
     problemtype = sim_params["scenario"]
     problem_bathy = sim_params["bathymetry"]
+    left_bc = sim_params["bc_file"]
+    store = False
 
     # Create SWE solver and calculate the solution
     solver = SWESolver(xbounds, timestep, nx, total_t,
                         tstart=tstart, g=g, kappa=kappa, dealias=dealias,
-                        problemtype=problemtype)
+                        problemtype=problemtype, bc_file=left_bc)
     x = solver.domain.x
 
     # Set up the bathymetry
@@ -51,32 +55,35 @@ def main():
         bathy = rampFunc(x)
 
     print("Start Simulation")
+    start = time.time()
     H_sensor, t_array, h_array, u_array = solver.solve(bathy)
-    print("Simulation Done")
+    end = time.time()
+    print(f"Simulation Done after {end-start}")
     dx = (xbounds[1]-xbounds[0])/nx
     # filename
-    filename = f"simulation_data_{problemtype}_{problem_bathy}.h5"
+    filename = f"simulation_data_{problemtype}_{problem_bathy}_test.h5"
     full_path = os.path.join(path, filename)
     print("Saving results to: ", full_path)
-    with h5py.File(full_path, "w") as f:
-        f.create_dataset("h", data=h_array)
-        f.create_dataset("u", data=u_array)
-        f.create_dataset("H_sensor", data=H_sensor)
-        f.create_dataset("b_exact", data=bathy)
-        f.create_dataset("xgrid", data=np.copy(solver.domain.x))
-        f.create_dataset("t_array", data=t_array)
-        f.attrs["T_N"] = total_t
-        f.attrs["xmin"] = xbounds[0]
-        f.attrs["xmax"] = xbounds[1]
-        f.attrs["tstart"] = tstart
-        f.attrs["dt"] = timestep
-        f.attrs["dx"] = dx
-        f.attrs["g"] = g
-        f.attrs["k"] = kappa
-        f.attrs["M"] = nx
-        if bathy_config["parametrized"]:
-            f.attrs["npeaks"] = bathy_config["npeaks"]
-            f.attrs["bathy_params"] = bathy_params
+    if store:
+        with h5py.File(full_path, "w") as f:
+            f.create_dataset("h", data=h_array)
+            f.create_dataset("u", data=u_array)
+            f.create_dataset("H_sensor", data=H_sensor)
+            f.create_dataset("b_exact", data=bathy)
+            f.create_dataset("xgrid", data=np.copy(solver.domain.x))
+            f.create_dataset("t_array", data=t_array)
+            f.attrs["T_N"] = total_t
+            f.attrs["xmin"] = xbounds[0]
+            f.attrs["xmax"] = xbounds[1]
+            f.attrs["tstart"] = tstart
+            f.attrs["dt"] = timestep
+            f.attrs["dx"] = dx
+            f.attrs["g"] = g
+            f.attrs["k"] = kappa
+            f.attrs["M"] = nx
+            if bathy_config["parametrized"]:
+                f.attrs["npeaks"] = bathy_config["npeaks"]
+                f.attrs["bathy_params"] = bathy_params
 
 if __name__ == "__main__":
     main()
