653e85b97a01b32e8396eecc0219641dc5efedec
Lars Stietz <lars.stietz@tuhh.de>
2025-04-25 13:44:12 +0200

    debug posterior plot
    Former-commit-id: 7395af95b6a33ac226c3dbb7d9cd6220d29e8df9

--- plot_lp.jl
+++ plot_lp.jl
@@ -8,7 +8,7 @@ lps = deserialize("log_posterior_values.jls")
 #grid_p = deserialize("log_posterior_grid.jls")
 μs = LinRange(1.5,15.0,100) # deserialize("log_posterior_ms.jls")
 σs = LinRange(0.0, 2.0, 50) # deserialize("log_posterior_ss.jls")
-p_grid = hcat([[μ, σ] for μ in μs for σ in σs]...)'
+p_grid = hcat([[μ, σ] for μ in μs for σ in σs]...)
 
 lp_mat = permutedims(reshape(lps, length(σs), length(μs)))
 #replace -Inf with NaN
@@ -16,26 +16,28 @@ lp_mat[lp_mat .== -Inf] .= NaN
 
 println("Plotting log-posterior")
 f = Figure()
-ax = Axis(f[1, 1]; title="Log Posterior", xlabel="μ", ylabel="σ", xticks=1.5:1.:15.0, yticks=0.0:0.1:2.0, palette=(color = [:red,:orange, :brown],))
-contourf!(ax,μs, σs, lp_mat; nan_color=:white, colorbar=true, levels=50)
+ax = Axis(f[1, 1]; title="Log Posterior", xlabel="μ", ylabel="σ", xticks=1.5:1.:15.0, yticks=0.0:0.1:2.0)
+cont = contourf!(ax,μs, σs, lp_mat; nan_color=:white, levels=50)
 contour!(ax, μs, σs, lp_mat; nan_color=:white,levels=49, linewidth=0.25, color=:white)
-
+Colorbar(f[1, 2], cont)
 files = readdir(chain_experiment)
 chains = filter(x -> occursin(r"chain_[0-9]+.jls", x), files)
 n_chains = length(chains)
-samples = [deserialize(joinpath(experiment, file)) for file in chains]
+samples = [deserialize(joinpath(chain_experiment, file)) for file in chains]
 
 println("Plot the MCMC samples")
 for (i,chain) in enumerate(samples)
     μ = chain[:, 1]
     σ = chain[:, 2]
     scatter!(ax, μ[1], σ[1])
-    lines!(ax, μ, σ, alpha=0.5, label="Chain $(i)")
+    lines!(ax, μ, σ, label="Chain $(i)")
 end
 
 println("Plotting max posterior")
 _, idmax = findmax(lps)
+println("Max Posterior: ", idmax)
 max_p = p_grid[:, idmax]
+println("Max Posterior: ", max_p)
 scatter!(ax, max_p[1], max_p[2], color=:black, label="Max Posterior")
 axislegend(ax)
 
