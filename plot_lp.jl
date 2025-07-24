2f229efd6235705a0c126822bf784ac572ffe4a8
Lars Stietz <cls1557@pc5.mat.tu-harburg.de>
2025-07-24 13:31:26 +0200

    update lp scan plots
    Former-commit-id: fc5ee8deb31eed7d1ee388f8cebd1da97c7759c2

--- plot_lp.jl
+++ plot_lp.jl
@@ -1,3 +1,7 @@
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
 using CairoMakie
 using Serialization
 using LaTeXStrings
@@ -10,7 +14,7 @@ lp_experiment = ARGS[1]
 
 lps = deserialize(joinpath(lp_experiment, "log_posterior_values.jls"))
 μs = deserialize(joinpath(lp_experiment, "log_posterior_grid_ms.jls"))
-σs = 0.5 .*deserialize(joinpath(lp_experiment, "log_posterior_grid_ss.jls"))
+σs = deserialize(joinpath(lp_experiment, "log_posterior_grid_ss.jls"))
 p_grid = hcat([[μ, σ] for μ in μs for σ in σs]...)
 
 line_colors = [RGBf(0.1216, 0.4667, 0.7059), RGBf(1.0, 0.498, 0.0549), RGBf(0.1725, 0.6275, 0.1725), RGBf(0.5804, 0.4039, 0.7412), RGBf(0.7373, 0.7412, 0.1333), RGBf(0.0902, 0.7451, 0.8118), RGBf(0.8902, 0.4667, 0.7608), RGBf(0.549, 0.3373, 0.2941), RGBf(0.8392, 0.1529, 0.1569), RGBf(0.498, 0.498, 0.498)]
@@ -27,7 +31,7 @@ end
 
 println("Plotting log-posterior")
 f = Figure(size=cm2px((width_cm, height_cm)), fontsize=30)
-ax = Axis(f[1, 1]; title=latexstring("\\pi_L(H|b)\\pi_\\mathrm{Prior}(b)"), xlabel=latexstring("\\mu"), ylabel=latexstring("\\sigma^2"), xticks=1.5:1.:15.0, yticks=0.0:0.1:1.0, titlesize=32)
+ax = Axis(f[1, 1]; title=latexstring("\\pi_L(H|b)\\pi_\\mathrm{Prior}(b)"), xlabel=latexstring("\\mu"), ylabel=latexstring("\\sigma^2"), xticks=1.5:1.:15.0, yticks=0.0:0.05:0.5, titlesize=32)
 cont = contourf!(ax,μs, σs, lp_mat; nan_color=:white, levels=50)
 contour!(ax, μs, σs, lp_mat; nan_color=:white,levels=49, linewidth=0.25, color=:white)
 Colorbar(f[1, 2], cont)
@@ -47,7 +51,7 @@ if length(ARGS) == 2
     println("Plot the MCMC samples")
     for (i,chain) in enumerate(samples)
         μ = chain[:, 1]
-        σ = 0.5 .*chain[:, 2]
+        σ = chain[:, 2]
         scatter!(ax, μ[1], σ[1], color=line_colors[i], markersize=12)
         lines!(ax, μ, σ, label="Chain $(i)", color=line_colors[i], linewidth=3)
     end
