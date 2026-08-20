6917b1a2fcfbdec027fe42c3f345c3627caaaa13
Lars Stietz <lars.stietz@tuhh.de>
2026-08-20 14:37:10 +0200

    update ppc

--- posterior_predictive_check.jl
+++ posterior_predictive_check.jl
@@ -5,14 +5,20 @@ using Plots
 using StatsPlots
 using Statistics
 using LaTeXStrings
-
+using MCMCChains
+using Distributions
 using Distributed
-addprocs(7)
-@everywhere begin
+
+parallel = false
+
+if parallel
+    addprocs(2)
+    @everywhere begin
+        using BathymetryReco
+    end
+else
     using BathymetryReco
 end
-
-
 
 include("my_theme.jl")
 theme(:custom_slides)
@@ -34,51 +40,68 @@ obs_config = config.obs_settings
 # Load the data
 if obs_config.real_data
     obs_data = load_observation(obs_config.path, sim_config.tstart, sim_config.tinterval)
-    adjoint_file = "data/results/experiment_adjoint.csv"
-    adjoint_label = "Adjoint Solution, NRMSE = 16.42"
 else
     obs_data, exact_b = load_toy_observation(obs_config.path, obs_config.noise_var, sensor_rate=obs_config.sensor_rate)
-    adjoint_file = "data/results/simulated_noise_adjoint.csv"
-    adjoint_label = "Adjoint Solution, NRMSE = 10.76"
 end
 
+obs_H = reshape(obs_data.H, (1, size(obs_data.H)...))
 
-@everywhere forward(params) = simulation(params, $sim_config, $obs_data)
+if parallel
+    @everywhere forward(params) = simulation(params, $sim_config, $obs_data)
+else
+    forward(params) = simulation(params, sim_config, obs_data)
+end
 
 burnin = 1000
-sample_number = 4000
+sample_number = size(chain,1) - burnin
 
 bathy = chain[burnin+1:burnin+sample_number,1:mcmc_config.dim]
 lp = chain[burnin+1:burnin+sample_number,mcmc_config.dim+1]
 ar = chain[burnin+1:burnin+sample_number,end]
 
-sensor_simulations = Array{Float64}(undef, length(obs_data.t), length(obs_data.x), sample_number)
+sensor_simulations = Array{Float64}(undef, sample_number, length(obs_data.t), length(obs_data.x))
 
-for i in 1:sample_number
-    println("Simulating for sample ", i, " with log-posterior ", lp[i], " and acceptance rate ", ar[i])
-    sim = forward(bathy[i, :])
-    sensor_simulations[:, :, i] = sim
+if parallel
+    @distributed for i in 1:sample_number
+        println("Simulating for sample ", i, " with log-posterior ", lp[i], " and acceptance rate ", ar[i])
+        sim = forward(bathy[i, :])
+        println(size(sim))
+        println(size(sensor_simulations[i, :, :]))
+        sensor_simulations[i, :, :] = sim
+    end
+else
+    for i in 1:sample_number
+        println("Simulating for sample ", i, " with log-posterior ", lp[i], " and acceptance rate ", ar[i])
+        @time sim = forward(bathy[i, :])
+        sensor_simulations[i, :, :] = sim
+    end
 end
 
+mean_pred = dropdims(mean(sensor_simulations, dims=1), dims=1)
 
-mean_pred = dropdims(mean(sensor_simulations, dims=3), dims=3)
+sim_chains = Chains(sensor_simulations)
 
-# 95% confidence interval (2.5% and 97.5%) across the sample dimension (3rd dim)
-# Resulting `q_low` and `q_high` are matrices with dimensions
-# (ntime, nposition) matching `mean_pred`.
-q_low = [quantile(view(sensor_simulations, t, p, :), 0.025) for t in 1:size(sensor_simulations,1), p in 1:size(sensor_simulations,2)]
-q_high = [quantile(view(sensor_simulations, t, p, :), 0.975) for t in 1:size(sensor_simulations,1), p in 1:size(sensor_simulations,2)]
+rel_l2_sim_error = dropdims(sqrt.(sum((sensor_simulations .- obs_H).^2, dims=2)) ./ sqrt.(sum((obs_H).^2, dims=2)), dims=2)
 
-rel_l2_sim_error = sqrt.(sum((sensor_simulations .- obs_data.H).^2, dims=1)) ./ sqrt.(sum((obs_data.H).^2, dims=1))
-
-mean_rel_l2_sim_error = dropdims(round.(mean(rel_l2_sim_error, dims=3).*100, digits=4), dims=3)
-std_rel_l2_sim_error = dropdims(round.(1.96.*(std(rel_l2_sim_error, dims=3))./sqrt(sample_number).*100, digits=4), dims=3)
+mean_rel_l2_sim_error = round.(mean(rel_l2_sim_error, dims=1).*100, digits=4)
+std_rel_l2_sim_error = round.((std(rel_l2_sim_error, dims=1))./sqrt(sample_number).*100, digits=4)
 
 for i in 1:3
-    ribbon = (mean_pred[:,1] .- q_low[:,1], q_high[:,1] .- mean_pred[:,1])
-    ppc_plot = plot(;title="Posterior Predictive Check", xlabel="time [s]", ylabel="Water surface height [m]")
-    plot!(ppc_plot, obs_data.t, obs_data.H; title="Sensor $i, ε=$(mean_rel_l2_sim_error[i]) +-$(std_rel_l2_sim_error[i])%", label="measurement", xlabel=L"t [s]", ylabel=L"H [m]", linestyle=:dash)
-    plot!(ppc_plot, obs_data.t, mean_pred[:,1]; ribbon=ribbon, label="mean prediction with 95% CI", linestyle=:dot, linewidth=2)
+    error_string = latexstring("\\varepsilon = $(mean_rel_l2_sim_error[i]) \\pm $(std_rel_l2_sim_error[i])\\%")
+
+    sensor_chain = sim_chains[i]
+    #ess = ess(sensor_chain)[:, :ess]
+    se = mcse(sensor_chain)[:, :mcse]
+    t_d = [quantile(TDist(10.- 1), 0.975) for n_e in ess]
+    se = se .* t_d
+    ci_low = hpd(sensor_chain)[:, :lower]
+    ci_high = hpd(sensor_chain)[:, :upper]
+
+    ribbon = (mean_pred[:,i] .- ci_low[:,i], ci_high[:,i] .- mean_pred[:,i])
+    ppc_plot = plot(;xlabel= L"t [\mathrm{s}]", ylabel= L"H [\mathrm{m}]")
+    plot!(ppc_plot, obs_data.t, obs_data.H[:,i]; title="Sensor $i, ε=$(mean_rel_l2_sim_error[i]) +-$(std_rel_l2_sim_error[i])%", label="measurement", linestyle=:dash)
+    plot!(ppc_plot, obs_data.t, mean_pred[:,i]; ribbon=ribbon, label="mean prediction with 95% CI", linestyle=:dot, linewidth=2)
+    scatter!(ppc_plot, obs_data.t, mean_pred[:,i], yerror=se, alpha=0.3, color=:blue)
     savefig(ppc_plot, joinpath(exp, "plots/sim_chain_sensor_$(i)_ci.png"))
     savefig(ppc_plot, joinpath(exp, "plots/sim_chain_sensor_$(i)_ci.pdf"))
 end
